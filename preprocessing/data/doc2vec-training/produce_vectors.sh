39c3112f38c9d6e64064008842ebed87f9deff56
ssephillip <phillip.sittigweb.de>
2019-09-28 14:48:58 +0200

    Bugfix in removing false vectors; increases the number of threads used

--- preprocessing/data/doc2vec-training/produce_vectors.sh
+++ preprocessing/data/doc2vec-training/produce_vectors.sh
@@ -17,7 +17,7 @@ gcc doc2vecc.c -o doc2vecc -lm -pthread -O3 -march=native -funroll-loops
 
 # this script trains on all the data (train/test/unsup), you could also remove the test documents from the learning of word/document representation
 echo "starting training"
-./doc2vecc -train $training_file_doc_only -word ./result/wordvectors.txt -output ./result/$result_vectors -cbow 1 -size 100 -window 10 -negative 5 -hs 0 -sample 0 -threads 4 -binary 0 -iter 20 -min-count 10 -test $training_file_doc_only -sentence-sample 0.1 -save-vocab ./result/alldata.vocab
+./doc2vecc -train $training_file_doc_only -word ./result/wordvectors.txt -output ./result/$result_vectors -cbow 1 -size 100 -window 10 -negative 5 -hs 0 -sample 0 -threads 8 -binary 0 -iter 20 -min-count 10 -test $training_file_doc_only -sentence-sample 0.1 -save-vocab ./result/alldata.vocab
 
-echo "deleting false vectors"
-sed -i '/^[0\. ]*$/d' ./result/$result_vectors #removes wrong vectors (consist only of 0)
+echo "deleting falsly produced vectors"
+sed -i '$ d' ./result/$result_vectors #removes last vector which was falsly produced
