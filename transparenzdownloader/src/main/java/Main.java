edd4b6cda24e49a54a372e68bdd5edf90b4b5e5c
ssephillip <phillip.sittigweb.de>
2019-10-20 17:49:15 +0200

    Moves Transparenz Reource Downloader to own project

---
+++ transparenzdownloader/src/main/java/Main.java
@@ -0,0 +1,27 @@
+import java.time.Duration;
+import java.time.Instant;
+
+public class Main {
+
+    public static void main(String[] args){
+        //example parameters: "http://localhost:8983/solr/simfin" "/home/phillip/BA/data/transparenz3/" 150000 10000 11000 3
+        String solrAddress = args[0];
+        String path = args[1];
+        int numOfDocs = Integer.valueOf(args[2]);
+        int startFrom = Integer.valueOf(args[3]);
+        int downloadUntil = Integer.valueOf(args[4]);
+        int numOfThreads =Integer.valueOf(args[5]);
+
+
+        TransparenzResourceDownloader transparenzResourceDownloader = new TransparenzResourceDownloader(solrAddress);
+        Instant start = Instant.now();
+        try {
+            transparenzResourceDownloader.download(path, numOfDocs, startFrom, downloadUntil, numOfThreads);
+        }catch(InstantiationException e){
+            System.out.println("Couldn't retrieve IDs!");
+            e.printStackTrace();
+        }
+        Instant end = Instant.now();
+        System.out.println("Downloaded all documents in "+ Duration.between(start,end).getSeconds()+" seconds");
+    }
+}
---
+++ transparenzdownloader/src/main/java/TpDocument.java
@@ -0,0 +1,123 @@
+import java.util.Objects;
+
+/**
+ * A TpDocument represents an inner document of the Transparenz Portal solr index.
+ * The solr index of the Transparenz Portal contains SolrDocuments (outer documents).
+ * An outer document is only a wrapper for the information of the actual document.
+ * This "actual document" is an inner document.
+ * An outer document may hold information about multiple inner documents.
+ * An outer document holds many lists with different information (e.g. file-formats, filename, fultexts).
+ * Each element in the list is the information of one inner document.
+ * The information is mapped to the inner document by the position in the list.
+ * E.g. For the inner document X the filename is in the position 2 of the list of filenames.
+ *      Therefore the file-format in position 2 in the list of file-formats is the file-format of this inner document etc.
+ */
+public class TpDocument {
+
+    /** The id of the outer document (possibly referencing multiple TpDocuments (inner documents)) */
+    String outerId;
+
+    /** The id of the inner document (represented by this object). */
+    String innerId; //TODO ps 2019-08-20: evtl. auf int umstellen
+
+    /** The URL to the original resource. */
+    String resUrl;
+
+    /** The fulltext of this (inner) document. */
+    String resFulltext;
+
+    /** The format of this (inner) document. */
+    String resFormat;
+
+
+    /** The outer document name. */
+    String title;
+
+    /** The inner document name */
+    String resName;
+
+    /** The date the document was published */
+    String date;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TpDocument)) return false;
+        TpDocument that = (TpDocument) o;
+        return Objects.equals(innerId, that.innerId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(innerId);
+    }
+
+    public String getOuterId() {
+        return outerId;
+    }
+
+    public void setOuterId(String outerId) {
+        this.outerId = outerId;
+    }
+
+    public String getInnerId() {
+        return innerId;
+    }
+
+    public void setInnerId(String innerId) {
+        this.innerId = innerId;
+    }
+
+    public String getResUrl() {
+        return resUrl;
+    }
+
+    public void setResUrl(String resUrl) {
+        this.resUrl = resUrl;
+    }
+
+    public String getResFulltext() {
+        return resFulltext;
+    }
+
+    public void setResFulltext(String resFulltext) {
+        this.resFulltext = resFulltext;
+    }
+
+    public String getResFormat() {
+        return resFormat;
+    }
+
+    public void setResFormat(String resFormat) {
+        this.resFormat = resFormat;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getResName() {
+        return resName;
+    }
+
+    public void setResName(String resName) {
+        this.resName = resName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+
+
+}
