b07b24c3c8385d02909ac2fb879228bcb70052a0
ssephillip <phillip.sittigweb.de>
2020-01-15 11:09:56 +0100

    Adjusts install scripts (testing still pending)

--- install-newsleak.sh
+++ install-newsleak.sh
@@ -5,8 +5,19 @@
 #3. docker-compose
 git clone https://github.com/ssephillip/newsleak-docker.git
 cd newsleak-docker
+mkdir -p volumes/ui/conf/dictionaries
+mkdir -p volumes/ui/data
+mkdir -p volumes/ui/doc2vec-training/result
+chmod 777 -R volumes/ui
 docker network create hoover_default
 docker-compose up -d
 docker exec -it -u root newsleak ./newsleak-start.sh
 docker-compose stop
+
+echo "--------------------------------------------------"
+echo "--------------------------------------------------"
+echo "--------------------------------------------------"
 echo "Setup finished. You can now start the application."
+echo "--------------------------------------------------"
+echo "--------------------------------------------------"
+echo "--------------------------------------------------"
--- newsleak-start.sh
+++ newsleak-start.sh
@@ -5,7 +5,11 @@ cd /opt/newsleak
 cp -n -r conf /etc/settings/
 cp -n -r data /etc/settings/
 cp -n -r doc2vec-training /etc/settings/
-#chmod 777 -R /etc/settings
+####
+#noch zu testen
+chmod 777 -R /etc/settings/
+chown -R newsleak:newsleak /etc/settings/
+####
 
 export NEWSLEAK_CONFIG=/etc/settings/conf/application.production.conf
 bin/newsleak -Dconfig.file=/etc/settings/conf/application.production.conf
