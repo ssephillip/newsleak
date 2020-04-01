ab2e1e244677a68f3cd83cfe5c6b3e77582de1da
ssephillip <phillip.sittigweb.de>
2020-04-01 18:48:59 +0200

    removes redundant time measurements and code that was commented out (and is not needed anymore); adds comments

--- preprocessing/src/main/java/uhh_lt/newsleak/preprocessing/DocEmbeddingManager.java
+++ preprocessing/src/main/java/uhh_lt/newsleak/preprocessing/DocEmbeddingManager.java
@@ -18,61 +18,55 @@ import java.io.InputStreamReader;
 import java.time.Instant;
 
 /**
- * Provides functionality to create document embedding vectors and store these vectors to a remote vector index (via WebService)
+ * Provides functionality to create document embedding vectors and store these vectors to a remote vector index (via an Http Request)
  */
 public class DocEmbeddingManager extends NewsleakPreprocessor {
 
-
+    //Creates Doc2VecC document embedding vectors and stores them in a remote vector index
     public static void main(String[] args) {
         DocEmbeddingManager docEmbeddingManager = new DocEmbeddingManager();
         docEmbeddingManager.getConfiguration(args);
-        StatsService.getInstance().startNewRun(docEmbeddingManager.dataDirectory, docEmbeddingManager.threads); //TODO wieder entfernen für finale JAR (gehört eigentlich nur in InformationExtraction2Postgres)
-        long startCreateEmbedding = System.currentTimeMillis();
+
         StatsService.getInstance().addStatsEvent(StatsService.EVENT_TYPE_START, StatsService.EMBEDDING, Instant.now());
         docEmbeddingManager.createEmbeddings();
         StatsService.getInstance().addStatsEvent(StatsService.EVENT_TYPE_STOP, StatsService.EMBEDDING, Instant.now());
-        long timeCreateEmbedding = System.currentTimeMillis()-startCreateEmbedding;
 
-        long startVectorIndexing = System.currentTimeMillis();
+
         StatsService.getInstance().addStatsEvent(StatsService.EVENT_TYPE_START, StatsService.EMBEDDING_INDEXING, Instant.now());
         docEmbeddingManager.indexVectors();
         StatsService.getInstance().addStatsEvent(StatsService.EVENT_TYPE_STOP, StatsService.EMBEDDING_INDEXING, Instant.now());
-        long timeVectorIndexing = System.currentTimeMillis()-startVectorIndexing;
-
-        System.out.println("Time spent for creating embeddings (seconds): " + timeCreateEmbedding / 1000);
-        System.out.println("Time spent for indexing the (vector) embeddings (seconds): " + timeVectorIndexing / 1000);
     }
 
 
+
     /**
-     * This method triggers a Shell script which prepares the training data and starts a C-Script which starts the embedding process.
+     * Triggers a Shell script which prepares the training data and starts a C-Program. This C-Program which starts the embedding process.
      * The resulting vectors are written to a textfile in the result directory.
      */
     public void createEmbeddings() {
 
+        //Intializes the process builder
         ProcessBuilder processBuilder = new ProcessBuilder();
         processBuilder.inheritIO();
         processBuilder.directory(new File(this.doc2vecTrainingDir));
-        //processBuilder.command("bash", "produce_vectors.sh "+this.trainingFileName+".txt "+this.trainingFileNameDocOnly+".txt "+this.trainingFileNameIdOnly+".txt "+this.doc2vecResultFileName +".txt");
 
+        //specifies the file to execute and the parameters
+        //TODO 2020-04-01 ps: define parameters is separate strings
+        //TODO 2020-04-01 ps: evtl. prepare training data within java
         processBuilder.command(this.doc2vecTrainingDir+File.separator+"produce_vectors.sh", this.trainingFileName+".txt", this.trainingFileNameDocOnly+".txt", this.trainingFileNameIdOnly+".txt", this.doc2vecResultFileName+".txt");
-        //processBuilder.command("bash produce_vectors.sh",this.trainingFileName+".txt",this.trainingFileNameDocOnly+".txt",this.trainingFileNameIdOnly+".txt",this.doc2vecResultFileName +".txt");
-
 
 
         try {
             logger.log(Level.INFO, "Starting document embedding process with "+this.trainingFileName+" as training data.");
             Process process = processBuilder.start();
 
-            //For some reason the script does not finish/return if the output isn't read
-            //For this reason we read the output here. The output is of no relevance, it is just a bug fix since I was not able to identify the problem.
-            StringBuilder output = readOutput(process);
-
+            StringBuilder output = readOutput(process); //For some reason the script does not finish/return if the output isn't read. For this reason we read the output here. The output is of no relevance. It is just a bug fix since I was not able to identify the problem.
 
             int exitVal = process.waitFor();
             if (exitVal != 0) {
                 throw new IllegalArgumentException();
             }
+
             logger.log(Level.INFO, "Finished document embedding process successfully.");
 
         }catch(Exception e){
@@ -91,19 +85,21 @@ public class DocEmbeddingManager extends NewsleakPreprocessor {
         CloseableHttpClient client = HttpClients.createDefault();
         HttpPost httpPost = new HttpPost(this.doc2vecIndexUrl+":"+this.doc2vecIndexPort+"/index_vectors");
 
-
-        //String pathToResultVectorFile =
+        //Constructs the Multipart and adds it to the http request
         MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+        //TODO 2020-04-01 ps: introduce separate string variables for parameters
         builder.addBinaryBody("vectors", new File(this.doc2vecResultDir+File.separator+this.doc2vecResultFileName +".txt"), ContentType.APPLICATION_OCTET_STREAM, doc2vecResultFileName +".ext"); //TODO 2019-09-18 ps: introduce own attribute in config file for result dir
         builder.addBinaryBody("ids", new File(this.doc2vecTrainingDir+File.separator+this.trainingFileNameIdOnly+".txt"), ContentType.APPLICATION_OCTET_STREAM, trainingFileNameIdOnly+".ext");//TODO 2019-09-18 ps: maybe copy id file also to result dir for consistency
         HttpEntity multipart = builder.build();
         httpPost.setEntity(multipart);
 
         try {
+            //executes http request
             logger.log(Level.INFO, "Starting document vector indexing. Index URL: "+httpPost.getURI());
             CloseableHttpResponse response = client.execute(httpPost);
             client.close();
 
+            //logs if the vectors were indexed successfully based on the http response code
             int statusCode = response.getStatusLine().getStatusCode();
             if(statusCode == 200) {
                 logger.log(Level.INFO, "Finished document vector indexing successfully.");
@@ -119,6 +115,12 @@ public class DocEmbeddingManager extends NewsleakPreprocessor {
     }
 
 
+    /**
+     * Method to read the output of a process
+     * @param process A process
+     * @return The output of the process
+     * @throws IOException
+     */
     private StringBuilder readOutput(Process process) throws IOException {
         StringBuilder output = new StringBuilder();
         BufferedReader reader = new BufferedReader(
